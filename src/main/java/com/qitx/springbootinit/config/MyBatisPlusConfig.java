dc8fdbf0c237f65b448ac2d15ec018e70f642bdf
Qitx14 <2197787573@qq.com>
2024-01-22 19:24:44 +0800

    springboot-init springboot 项目模板

--- src/main/java/com/qitx/springbootinit/config/MyBatisPlusConfig.java
+++ src/main/java/com/qitx/springbootinit/config/MyBatisPlusConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 @Configuration
-@MapperScan("com.yupi.springbootinit.mapper")
+@MapperScan("com.qitx.springbootinit.mapper")
 public class MyBatisPlusConfig {
 
     /**

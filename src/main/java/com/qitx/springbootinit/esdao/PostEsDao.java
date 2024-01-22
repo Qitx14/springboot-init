c1daf995a61d725263c0a5fe0f41736979793dcd
Qitx14 <2197787573@qq.com>
2024-01-22 19:06:34 +0800

    springboot-init springboot 项目模板

---
+++ src/main/java/com/qitx/springbootinit/esdao/PostEsDao.java
@@ -0,0 +1,15 @@
+package com.qitx.springbootinit.esdao;
+
+import com.qitx.springbootinit.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ *
+
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
